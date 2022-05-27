ced4f5617be033adb9f051ab1cdf6035a659706d
nafees nazik <nafeesnazik21@gmail.com>
2022-05-27 20:01:23 +0530

    fix: classname issue

--- src/components/clipboard-button.tsx
+++ src/components/clipboard-button.tsx
@@ -17,12 +17,12 @@ export const ClipboardButton: Component<{ value: string }> = (props) => {
       <svg
         aria-hidden={true}
         xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
+        class="h-5 w-5"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
         stroke-width={2}
-        classList={{ hidden: copied() }}
+        classList={{ "hidden ": copied() }}
       >
         <path
           stroke-linecap="round"
@@ -33,12 +33,12 @@ export const ClipboardButton: Component<{ value: string }> = (props) => {
       <svg
         aria-hidden={true}
         xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
+        class="h-5 w-5"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
         stroke-width={2}
-        classList={{ hidden: !copied() }}
+        classList={{ "hidden ": !copied() }}
       >
         <path
           stroke-linecap="round"
