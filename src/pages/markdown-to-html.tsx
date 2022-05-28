fa11888efef8422dd73d6ff4471f5d3e929b3467
nafees nazik <nafeesnazik21@gmail.com>
2022-05-28 11:54:40 +0530

    feat: add sample button

--- src/pages/markdown-to-html.tsx
+++ src/pages/markdown-to-html.tsx
@@ -6,7 +6,35 @@ import {
   ErrorMessage,
   TextArea,
   ClipboardButton,
+  Button,
 } from "../components";
+
+const sample = `
+
+Heading
+=======
+## Sub-heading
+Paragraphs are separated
+by a blank line.
+Two spaces at the end of a line
+produces a line break.
+Text attributes _italic_,
+**bold**, \`monospace\`.
+Horizontal rule:
+---
+Bullet list:
+  * apples
+  * oranges
+  * pears
+Numbered list:
+  1. wash
+  2. rinse
+  3. repeat
+A [link](http://example.com).
+![Image](https://via.placeholder.com/150)
+> Markdown uses email-style > characters for blockquoting.
+
+`;
 
 export const MarkdownToHtml: Component = () => {
   const [input, setInput] = createSignal("");
@@ -31,7 +59,12 @@ export const MarkdownToHtml: Component = () => {
     <TwoColumns>
       <Column
         title="Markdown"
-        renderRight={() => <ClipboardButton value={input()} />}
+        renderRight={() => (
+          <div class="flex space-x-3">
+            <Button onClick={() => setInput(sample)}>sample</Button>
+            <ClipboardButton value={input()} />
+          </div>
+        )}
       >
         <TextArea
           oninput={(e) => setInput(e.currentTarget.value)}
