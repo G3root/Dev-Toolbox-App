707616d7aa44a06eaa41a38d84812cb209444d14
nafees nazik <nafeesnazik21@gmail.com>
2022-05-28 11:50:10 +0530

    style: add disabled style

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -6,7 +6,7 @@ interface TextAreaProps
 export const TextArea: Component<TextAreaProps> = (props) => {
   return (
     <textarea
-      class="w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 mt-1 block w-full text-sm border border-gray-300 rounded-md"
+      class="w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 mt-1 block w-full text-sm border border-gray-300 rounded-md disabled:cursor-not-allowed"
       {...props}
     />
   );
