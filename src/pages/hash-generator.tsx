2c8373c9708865e2e76ce1cf5e9650226e605ff4
nafees nazik <nafeesnazik21@gmail.com>
2022-06-05 10:51:30 +0530

    feat: update HashGenerator page

--- src/pages/hash-generator.tsx
+++ src/pages/hash-generator.tsx
@@ -1,9 +1,52 @@
-import type { Component } from "solid-js";
+import { Component, createEffect, createSignal, For, Show } from "solid-js";
+import { invoke } from "@tauri-apps/api/tauri";
+import {
+  ClipboardButton,
+  CodeBlock,
+  Column,
+  TextArea,
+  TwoColumns,
+} from "../components";
+import { NameValueResp } from "../types";
 
 export const HashGenerator: Component = () => {
+  const [input, setInput] = createSignal("");
+  const [value, setValue] = createSignal<NameValueResp>([]);
+
+  createEffect(async () => {
+    if (input() !== "") {
+      const res = (await invoke("hash_generator", {
+        text: input(),
+      })) as NameValueResp;
+      setValue(res);
+    }
+  });
   return (
-    <div>
-      <p class="text-4xl text-green-700 text-center py-20">Hello world !</p>
-    </div>
+    <TwoColumns>
+      <Column title="Input">
+        <TextArea
+          value={input()}
+          rows={10}
+          onInput={(e) => setInput(e.currentTarget.value)}
+        />
+      </Column>
+      <Column title="Result">
+        <Show when={input() !== ""}>
+          <div class="grid grid-cols-1 gap-8">
+            <For each={value()}>
+              {(item) => (
+                <div>
+                  <div class="mb-3 flex items-center justify-between">
+                    <span>{item.name}</span>
+                    <ClipboardButton value={item.value} />
+                  </div>
+                  <CodeBlock code={item.value} />
+                </div>
+              )}
+            </For>
+          </div>
+        </Show>
+      </Column>
+    </TwoColumns>
   );
 };
